e6e22732cc707d4ecb3bd9b6c8cebbd225da965e
Brandon Till <btill730@gmail.com>
2022-04-05 18:19:15 -0700

    in progress

--- src/Map.js
+++ src/Map.js
@@ -4,7 +4,26 @@ import {BLOCK_SIZE, MAP_WIDTH, MAP_HEIGHT} from "./constants";
 import TerrainBlock from "./TerrainBlock";
 
 const Map = () => {
-    const mapArray = Array(MAP_HEIGHT).fill(Array(MAP_WIDTH).fill({Type:'Plain', Defense:1}));
+    const mapArray = Array(MAP_HEIGHT).fill(Array(MAP_WIDTH).fill({Type:'Plain', Defense: 1}));
+    const junko = require('./Map1.JSON')
+    //console.log(Object.keys(junko))
+    //let obj = JSON.parse(junko)
+    const mapEdits = Object.keys(junko).map(key => junko[key]);
+    //console.log
+
+    //Type, defence, row, col?
+    while(mapEdits.size > 0){
+        let bloc = mapEdits.shift();
+        console.log(bloc)
+        let type = bloc.Type;
+        let defence = bloc.Defence;
+        let row = bloc.Row;
+        let col = bloc.Column;
+        mapArray[row][col].Type = type;
+        mapArray[row][col].Defense = defence;
+    }
+
+
     return (
         <Fragment>
             <Box sx={{width: MAP_WIDTH * BLOCK_SIZE, height: MAP_HEIGHT * BLOCK_SIZE}}>
--- src/TerrainBlock.js
+++ src/TerrainBlock.js
@@ -3,10 +3,18 @@ import {Box} from '@mui/material';
 import {BLOCK_SIZE} from "./constants";
 import plain from './images/terrain/plain32.png'
 import water from './images/terrain/water32.png'
+import redHQ from './images/terrain/HQred.png'
+import blueHQ from './images/terrain/HQblue.png'
+import redFactory from './images/terrain/FactoryRed.png'
+import blueFactory from './images/terrain/FactoryBlue.png'
 
 const TerrainBlock = (props) => {
     let src;
-    if(props.type === 'Plain') src = plain;
+    let defense = 0;
+    if(props.type === 'Plain') {
+        src = plain;
+        defense = 1;
+    }
     if(props.type === 'Water') src = water;
     return (
         <Fragment>
